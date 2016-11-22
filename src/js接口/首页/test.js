51ff1bcf4355fafdd73a171b00f3f4cffca57040
lymava <lymava@sina.com>
2016-11-22 15:49:32 +0800

    展览详情

--- src/js接口/首页/test.js
+++ src/js接口/首页/test.js
@@ -1,21 +1,40 @@
 var res = {
-	"data" : [ {
-		"picname" : "attachFiles/temp/581b0915d6c45925f82e4915",
-		"realname" : "张景岳",
-		"artistCard_id" : "5815bbafd6c459747a637851",
-		"artist_id" : "581b0916d6c45925f82e4916",
-		"pic" : "attachFiles/20161030/5815bbafd6c459747a637851/5815bbaed6c459747a637850",
-		"pic_back" : "attachFiles/20161030/5815bbafd6c459747a637851/5815bbaed6c459747a637850",
-		"intro" : "MTIzdTg5Zmhkc2FmaGRza2FqbA==",
+	"data" : {
+		"id" : "581ef1c8d6c4594f90fa046e",
+		"pub_id" : "581ef1c8d6c4594f90fa046e",
+		"name" : "艺术馆1",
+		"intro" : "566A5LuL566A5LuL566A",
 		"state" : "1",
-		"viewcount" : "1",
-		"nice_count" : "1",
-		"shoucang_count" : "1"
-	} ],
-	"page" : "1",
-	"lastPage" : "1",
-	"pageSize" : "15",
-	"count" : "1",
+		"viewcount" : "7",
+		"nice_count" : "0",
+		"shoucang_count" : "1",
+		"pic" : "attachFiles/20161106/581ef1c8d6c4594f90fa046e/5833be32ef722c0f39aa761a",
+		"showTime" : "2016-11-06 17:03:04",
+		"time" : "1478422984000",
+		"rootPubConlumnId" : "57fca56def722c216b767c91",
+		"rootPubConlumnName" : "内容管理",
+		"secondPubConlumnId" : "581ef1a1d6c4594f90fa046c",
+		"secondPubConlumnName" : "艺术馆",
+		"comment_list" : [],
+		"logo" : "attachFiles/temp/5833be56ef722c0f39aa761c",
+		"jubanfang" : "江渝会馆",
+		"zhanlan_array" : [ {
+			"name" : "展览名称!",
+			"intro" : "5bGV6KeI566A5LuL",
+			"pic" : "attachFiles/20161106/581ef1c8d6c4594f90fa046e/581ef1c3d6c4594f90fa046d",
+			"zhanlan_zhanpin" : [
+					{
+						"name" : "山水画",
+						"intro" : "5bGx5rC055S7566A5LuL",
+						"pic" : "attachFiles/20161106/581ef1c8d6c4594f90fa046e/581ef1c3d6c4594f90fa046d"
+					},
+					{
+						"name" : "山水画",
+						"intro" : "5bGx5rC055S7566A5LuL",
+						"pic" : "attachFiles/20161106/581ef1c8d6c4594f90fa046e/581ef1c3d6c4594f90fa046d"
+					} ]
+		} ]
+	},
 	"statusCode" : "200",
 	"message" : "返回成功!"
 }
