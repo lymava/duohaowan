3211108cc2e4b65146ee2bd72aaee864f8d2cde0
lymava <lymava@sina.com>
2016-11-22 16:19:51 +0800

    展馆详情

--- src/js接口/首页/展馆详情.js
+++ src/js接口/首页/展馆详情.js
@@ -29,12 +29,12 @@ var result = {
 			"id" : "581ef1c8d6c4594f90fa046e",
 			"pub_id" : "581ef1c8d6c4594f90fa046e",
 			"name" : "艺术馆1",
-			"intro" : "566A5LuL566A5LuL566A5LuL566A5LuL566A5LuL566A5LuL566A5LuL566A5LuL566A5LuL566A5LuL566A5LuL566A5LuL566A5LuL566A5LuL566A5LuL566A5LuL566A5LuL566A5LuL566A5LuL566A5LuL566A5LuL566A5LuL566A5LuL566A5LuL566A5LuL566A5LuL566A5LuL566A5LuL566A5LuL566A5LuL566A5LuL566A5LuL566A5LuL566A5LuL566A5LuL566A5LuL566A5LuL566A5LuL566A5LuL566A5LuL566A5LuL566A5LuL",
+			"intro" : "5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL5bGV6aaG566A5LuL",
 			"state" : "1",
 			"viewcount" : "0",
 			"nice_count" : "0",
-			"shoucang_count" : "1",
-			"pic" : "attachFiles/20161106/581ef1c8d6c4594f90fa046e/5833be32ef722c0f39aa761a",
+			"shoucang_count" : "3",
+			"pic" : "attachFiles/20161106/581ef1c8d6c4594f90fa046e/581ef1c3d6c4594f90fa046d",
 			"showTime" : "2016-11-06 17:03:04",
 			"time" : "1478422984000",
 			"rootPubConlumnId" : "57fca56def722c216b767c91",
@@ -42,8 +42,39 @@ var result = {
 			"secondPubConlumnId" : "581ef1a1d6c4594f90fa046c",
 			"secondPubConlumnName" : "艺术馆",
 			"comment_list" : [],
-			"logo" : "attachFiles/temp/5833be56ef722c0f39aa761c",
-			"jubanfang" : "江渝会馆"
+			"logo" : "attachFiles/temp/5833e0a7d6c4592b41d886e4",
+			"jubanfang" : "举办方",
+			"zhanlan_array" : [ {
+				"name" : "展览名称!",
+				"intro" : "5bGV6KeI566A5LuL",
+				"pic" : "attachFiles/20161106/581ef1c8d6c4594f90fa046e/581ef1c3d6c4594f90fa046d",
+				"zhanlan_zhanpin" : [
+						{
+							"name" : "山水画",
+							"intro" : "5bGx5rC055S7566A5LuL",
+							"pic" : "attachFiles/20161106/581ef1c8d6c4594f90fa046e/581ef1c3d6c4594f90fa046d"
+						},
+						{
+							"name" : "山水画",
+							"intro" : "5bGx5rC055S7566A5LuL",
+							"pic" : "attachFiles/20161106/581ef1c8d6c4594f90fa046e/581ef1c3d6c4594f90fa046d"
+						},
+						{
+							"name" : "山水画",
+							"intro" : "5bGx5rC055S7566A5LuL",
+							"pic" : "attachFiles/20161106/581ef1c8d6c4594f90fa046e/581ef1c3d6c4594f90fa046d"
+						},
+						{
+							"name" : "山水画",
+							"intro" : "5bGx5rC055S7566A5LuL",
+							"pic" : "attachFiles/20161106/581ef1c8d6c4594f90fa046e/581ef1c3d6c4594f90fa046d"
+						},
+						{
+							"name" : "山水画",
+							"intro" : "5bGx5rC055S7566A5LuL",
+							"pic" : "attachFiles/20161106/581ef1c8d6c4594f90fa046e/581ef1c3d6c4594f90fa046d"
+						} ]
+			} ]
 		},
 		"statusCode" : "200",
 		"message" : "返回成功!"
